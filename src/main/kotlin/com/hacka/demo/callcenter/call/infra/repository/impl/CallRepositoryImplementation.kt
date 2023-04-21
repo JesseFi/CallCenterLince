6e7df396957801bb1ee74c77439e1983b35729b9
ruan.teske <ruan.teske@lince.com.br>
2023-04-21 16:19:07 -0300

    call create

--- src/main/kotlin/com/hacka/demo/callcenter/call/infra/repository/impl/CallRepositoryImplementation.kt
+++ src/main/kotlin/com/hacka/demo/callcenter/call/infra/repository/impl/CallRepositoryImplementation.kt
@@ -3,13 +3,32 @@ package com.hacka.demo.callcenter.call.infra.repository.impl
 import com.hacka.demo.callcenter.call.domain.entities.Call
 import com.hacka.demo.callcenter.call.domain.repository.CallRepository
 import com.hacka.demo.callcenter.call.domain.usecases.response.CallFilter
+import com.hacka.demo.callcenter.call.infra.repository.database.CallDatabase
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
 class CallRepositoryImplementation : CallRepository {
     override fun create(call: Call): Call? {
-        TODO("Not yet implemented")
+        var idGenerated = UUID.randomUUID()
+        call.uuid = idGenerated
+        return transaction {
+            CallDatabase.insert {
+                it[uuid] = call.uuid!!
+                it[numberCall] = call.numberCall!!
+                it[title] = call.title!!
+                //it[flow] = flow.uuid!!
+                it[contact] = call.contact!!
+                it[priority] = call.priority!!
+                //it[createDate] = call.createDate!!
+                it[author] = call.author!!
+                it[originProblemN] = call.originProblemN!!
+                it[originProblemS] = call.originProblemS!!
+            }.resultedValues!!
+            call
+        }
     }
 
     override fun listCalls(page: Int, size: Int, sorby: Int, orderBy: Int, callFilter: CallFilter): Call? {
